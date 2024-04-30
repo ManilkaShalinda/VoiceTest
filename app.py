6f6cddba707f5c51f477185b968ea3d4b165ebb1
ManilkaShalinda <manilkar98@gmail.com>
2024-04-30 11:54:14 +0530

    new

--- app.py
+++ app.py
@@ -1,58 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug  3 21:11:12 2020
-
-@author: pattn
-"""
-
-
-# coding: utf-8
-
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier 
-from sklearn import metrics
 from flask import Flask, request, render_template
-import pickle
+import pandas as pd
+import joblib
 
-app = Flask("__name__")
+app = Flask(__name__)
 
+# Load the trained model
+model = joblib.load('multi_label_rf_model.joblib')
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-q = ""
-
-@app.route("/")
-def loadPage():
-	return render_template('home.html', query="")
-
-
-@app.route("/predict", methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
+    # Get user input from the form
+    time = int(request.form['time'])
+    score = int(request.form['score'])
+    word_count = int(request.form['word_count'])
     
-    inputQuery1 = request.form['query1']
-    inputQuery2 = request.form['query2']
-    inputQuery3 = request.form['query3']
-    inputQuery4 = request.form['query4']
-    inputQuery5 = request.form['query5']
+    # Make a prediction using the loaded model
+    sample_data = pd.DataFrame({
+        'Time (seconds)': [time],
+        'Score': [score],
+        'Word Count': [word_count]
+    })
+    predicted_categories = model.predict(sample_data)
+    
+    # Convert predicted categories to human-readable format
+    categories = []
+    if predicted_categories[0][0] == 1:
+        categories.append("Fruits")
+    if predicted_categories[0][1] == 1:
+        categories.append("Vegetables")
+    if predicted_categories[0][2] == 1:
+        categories.append("Animals")
+    if predicted_categories[0][3] == 1:
+        categories.append("Commands")
+    
+    # Render the result template with predicted categories
+    return render_template('result.html', categories=categories)
 
-    model = pickle.load(open("model.sav", "rb"))
-    
-    
-    data = [[inputQuery1, inputQuery2, inputQuery3, inputQuery4, inputQuery5]]
-    new_df = pd.DataFrame(data, columns = ['texture_mean', 'perimeter_mean', 'smoothness_mean', 'compactness_mean', 'symmetry_mean'])
-    
-    single = model.predict(new_df)
-    probablity = model.predict_proba(new_df)[:,1]
-    
-    if single==1:
-        o1 = "The patient is diagnosed with Breast Cancer"
-        o2 = "Confidence: {}".format(probablity*100)
-    else:
-        o1 = "The patient is not diagnosed with Breast Cancer"
-        o2 = "Confidence: {}".format(probablity*100)
-        
-    return render_template('home.html', output1=o1, output2=o2, query1 = request.form['query1'], query2 = request.form['query2'],query3 = request.form['query3'],query4 = request.form['query4'],query5 = request.form['query5'])
-    
-if __name__ == "__main__":
-    app.run()
-
+if __name__ == '__main__':
+    app.run(debug=True)
